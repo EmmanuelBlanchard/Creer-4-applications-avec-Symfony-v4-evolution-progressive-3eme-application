c98119c9b79fe9474c5d1e1a711df3b15118ca5a
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-11 21:18:21 +0100

    La troisieme application, 8-Modification et formulaire : etape 2

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Aliment;
 use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,9 +27,17 @@ class AdminAlimentController extends AbstractController
     /**
      * @Route("/admin/aliment/{id}", name="admin_aliment_modification")
      */
-    public function modification(Aliment $aliment): Response
+    public function modification(Aliment $aliment,Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(AlimentType::class,$aliment);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($aliment);
+            $entityManager->flush();
+            return $this->redirectToRoute("admin_aliments");
+        }
+
         return $this->render('admin/admin_aliment/modificationAliment.html.twig', [
             'aliment' => $aliment,
             'form' => $form->createView()
